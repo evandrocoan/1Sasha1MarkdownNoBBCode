2bc67e1f6bf08cb34b2f75e5c50305d7c0c4c699
evandrocoan <evandrocoan@hotmail.com>
2016-11-15 20:35:14 -0200

    Renamed the `s` variable to `sourceCode` on MarkdownToBBCode.py

--- MarkdownToBBCode.py
+++ MarkdownToBBCode.py
@@ -15,65 +15,66 @@ class MarkdowntobbcodeCommand(sublime_plugin.TextCommand):
 		regionString = self.markdown_to_bbcode(str(self.view.substr(allcontent)))
 		self.view.replace(edit, allcontent, regionString)
 
-	def markdown_to_bbcode(self,s):
+	def markdown_to_bbcode(self,sourceCode):
 
 		def translate(p="%s", g=1):
+
 			def inline(m):
-				s = m.group(g)
+				sourceCode = m.group(g)
 				#
 				# Headers
 				#
-				s = re.sub(r"^#\s+(.*?)\s*$", "[h1]\\1[/h1]", s)   # # Header first level
-				s = re.sub(r"^##\s+(.*?)\s*$", "[h2]\\1[/h2]", s)  # ## Header second level
-				s = re.sub(r"^###\s+(.*?)\s*$", "[h3]\\1[/h3]", s) # ### Header third level
-				s = re.sub(r"^####\s+(.*?)\s*$", "[h4]\\1[/h4]", s)# #### Header fourth level
+				sourceCode = re.sub(r"^#\s+(.*?)\s*$", "[h1]\\1[/h1]", sourceCode)   # # Header first level
+				sourceCode = re.sub(r"^##\s+(.*?)\s*$", "[h2]\\1[/h2]", sourceCode)  # ## Header second level
+				sourceCode = re.sub(r"^###\s+(.*?)\s*$", "[h3]\\1[/h3]", sourceCode) # ### Header third level
+				sourceCode = re.sub(r"^####\s+(.*?)\s*$", "[h4]\\1[/h4]", sourceCode)# #### Header fourth level
 				#
 				# Lists
 				#
-				s = re.sub(r"(?m)^[-+*]\s+(.*)$", translate("№[list]\n[*]%s\n[/list]"), s) # + Marked + List
-				s = re.sub(r"(?m)^\d+\.\s+(.*)$", translate("№[list=1]\n[*]%s\n[/list]"), s) # 1. Numbered 2. List
+				sourceCode = re.sub(r"(?m)^[-+*]\s+(.*)$", translate("№[list]\n[*]%s\n[/list]"), sourceCode) # + Marked + List
+				sourceCode = re.sub(r"(?m)^\d+\.\s+(.*)$", translate("№[list=1]\n[*]%s\n[/list]"), sourceCode) # 1. Numbered 2. List
 				#
 				# Quote
 				#
-				s = re.sub(r"^> (.*)$", "[quote]\\1[/quote]", s) # > Quote
+				sourceCode = re.sub(r"^> (.*)$", "[quote]\\1[/quote]", sourceCode) # > Quote
 				#
 				# Thematic break
 				#
-				s = re.sub(r"^-{3}(\s*)$", "[hr]", s)
-				return p % s
+				sourceCode = re.sub(r"^-{3}(\s*)$", "[hr]", sourceCode)
+				return p % sourceCode
 			return inline
 
 		#
 		# URL and images
 		#
-		s = re.sub(r"!\[(.*?)\]\((.*?)\)", "[img]\\2[/img]", s)   # ![IMG description](URL of image), alt attribute not supported in many forums
-		s = re.sub(r"\[(.*?)\]\((.*?)\)", "[url=\\2]\\1[/url]", s)# [URL description](URL link)
-		s = re.sub(r"<(https?:\S+)>", "[url]\\1[/url]", s)        # <URL>
+		sourceCode = re.sub(r"!\[(.*?)\]\((.*?)\)", "[img]\\2[/img]", sourceCode)   # ![IMG description](URL of image), alt attribute not supported in many forums
+		sourceCode = re.sub(r"\[(.*?)\]\((.*?)\)", "[url=\\2]\\1[/url]", sourceCode)# [URL description](URL link)
+		sourceCode = re.sub(r"<(https?:\S+)>", "[url]\\1[/url]", sourceCode)        # <URL>
 		#
-		# Code
+		# Code, create a function for this and an Automated Unit Test
 		#
-		s = re.sub(r"`{3}([^`]+)`{3}", "[code]\\1[/code]", s)# ```Multiline\n\code```
-		s = re.sub(r"`([^`]+)`", "[code]\\1[/code]", s)      # `Code`
-		s = re.sub(r"(?m)^ {4}(.*)$", "№[code]\\1[/code]", s)# Code fragment after 4 spaces
-		s = re.sub(r"(?m)^\t(.*)$", "№[code]\\1[/code]", s)  # Code fragment after tab
+		sourceCode = re.sub(r"```([^`]+)```" , "[code]\\1[/code]" , sourceCode) # ```Multiline\n\code```
+		sourceCode = re.sub(r"`([^`]+)`"     , "[code]\\1[/code]" , sourceCode) # `Code`
+		sourceCode = re.sub(r"(?m)^ {4}(.*)$", "№[code]\\1[/code]", sourceCode) # Code fragment after 4 spaces
+		sourceCode = re.sub(r"(?m)^\t(.*)$"  , "№[code]\\1[/code]", sourceCode) # Code fragment after tab
 		#
 		# Bold and italic
 		#
-		s = re.sub(r"_{2}([\s\S]+?)_{2}", "[b]\\1[/b]", s)  # __Bold__
-		s = re.sub(r"_([^_]+?)_", "[i]\\1[/i]", s)   		# _Italic_ Needs hack (?<=\s), because _ symbol often use in URLs
-		s = re.sub(r"\*{2}([\s\S]+?)\*{2}", "[b]\\1[/b]", s)# **Bold**
-		s = re.sub(r"\*([^\*]+?)\*", "[i]\\1[/i]", s)       # *Italic*.
+		sourceCode = re.sub(r"_{2}([\s\S]+?)_{2}", "[b]\\1[/b]", sourceCode)  # __Bold__
+		sourceCode = re.sub(r"_([^_]+?)_", "[i]\\1[/i]", sourceCode)   		# _Italic_ Needs hack (?<=\s), because _ symbol often use in URLs
+		sourceCode = re.sub(r"\*{2}([\s\S]+?)\*{2}", "[b]\\1[/b]", sourceCode)# **Bold**
+		sourceCode = re.sub(r"\*([^\*]+?)\*", "[i]\\1[/i]", sourceCode)       # *Italic*.
 		#
 		# Strikethrough text
 		#
-		s = re.sub(r"~{2}([\s\S]+?)~{2}", "[s]\\1[\s]", s)
+		sourceCode = re.sub(r"~{2}([\s\S]+?)~{2}", "[sourceCode]\\1[\sourceCode]", sourceCode)
 		#
 		# Dependencies. Not delete these lines!
 		#
-		s = re.sub(r"(?m)^((?!№).*)$", translate(), s)
-		s = re.sub(r"(?m)^№\[", "[", s)
-		s = re.sub(r"\[/code]\n\[code(=.*?)?]", "\n", s)
-		s = re.sub(r"\[/list]\n\[list(=1)?]\n", "", s)
-		s = re.sub(r"\[/quote]\n\[quote]", "\n", s)
+		sourceCode = re.sub(r"(?m)^((?!№).*)$", translate(), sourceCode)
+		sourceCode = re.sub(r"(?m)^№\[", "[", sourceCode)
+		sourceCode = re.sub(r"\[/code]\n\[code(=.*?)?]", "\n", sourceCode)
+		sourceCode = re.sub(r"\[/list]\n\[list(=1)?]\n", "", sourceCode)
+		sourceCode = re.sub(r"\[/quote]\n\[quote]", "\n", sourceCode)
 
-		return s
+		return sourceCode
